f29481b32e3a0b727cde776f9275163a15a475e3
pavelpravchev <pavel.pravchev@gmail.com>
2014-09-03 16:51:50 +0300

    addition to list users

---
+++ application/forms/listusers.php
@@ -0,0 +1,21 @@
+<?php
+
+    return array(
+        'title' => Yii::t('application', 'Please provide your login credentials.'),
+
+        'elements' => array(
+            'search' => array(
+                'type' => 'text',
+                'maxlength' => 64,
+                'class' => 'form-control',
+                'hint' => 'Please enter your username',
+                // 'label' => 'Username'
+            ),
+        ),
+            'buttons' => array(
+                'submit' => array(
+                    'type' => 'submit',
+                    'label' => Yii::t('application', 'Find user')
+                )
+            )
+        );
---
+++ application/models/form/ListUsers.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace application\models\form;
+
+    use \Yii;
+    use \CException;
+    use \application\components\FormModel;
+
+    class ListUsers extends FormModel{
+        public $search;
+        public function rules(){
+            return array(
+                array('search', 'required'),
+                array('search', 'length', 'max' => 64),
+            );
+        }
+    }

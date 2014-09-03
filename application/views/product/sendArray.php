28e08b5866c62089a884ca615d98451f36282376
pavelpravchev <pavel.pravchev@gmail.com>
2014-09-03 11:22:40 +0300

    Update to pdf url

--- application/views/product/sendArray.php
+++ application/views/product/sendArray.php
@@ -2,14 +2,17 @@
 	use \application\models\db\Product;
 	// if (isset($_POST['send'] && strlen($_POST['send']))>0){
 		$baseURL = Yii::app()->baseUrl;
+		$assetMgr = Yii::app()->assetManager;
+
 		$products = Product::model()->findAllByAttributes(array('catId'=>$_POST['send']));
 		$prod = [];
 		foreach ($products as $product){
 			$prod[] = array(
 							array( 0 => $product->name, 1 => $product->model_number, 2=>$product->short_desc),
-							array( 0 => $product->long_desc, 1 => $product->pdf->url, 2 => $baseURL.'/product/viewProduct?id='.$product->id)
+							array( 0 => $product->long_desc, 1 => $assetMgr->publish($product->pdf->url), 2 => $baseURL.'/product/viewProduct?id='.$product->id)
 						);
 		}
 		$productJSON = json_encode($prod);
 		echo $productJSON;
 	// }
+		

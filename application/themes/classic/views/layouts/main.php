79cfe2ef5c24ed7d71b35e344e399d2104d44156
Clive Dann <clive.dann@googlemail.com>
2014-09-09 15:13:51 +0100

    Home page redesign begun

--- application/themes/classic/views/layouts/main.php
+++ application/themes/classic/views/layouts/main.php
@@ -1,8 +1,8 @@
 <?php
-    /**
-     * @var Controller $this
-     */
-    $assetUrl = Yii::app()->assetManager->publish(Yii::app()->theme->basePath . '/assets');
+/**
+* @var Controller $this
+*/
+$assetUrl = Yii::app()->assetManager->publish(Yii::app()->theme->basePath . '/assets');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,8 +14,8 @@
         ?>
         <link rel="stylesheet" type="text/css" href="<?php echo $bootstrap; ?>/css/bootstrap.min.css" media="all" />
         <!-- <link rel="stylesheet" type="text/css" href="<?php // echo Yii::app()->assetManager->publish(Yii::getPathOfAlias('themes.classic.assets') . '/css/styles.css'); ?>" media="all" /> -->
-        <!--<script src="https://code.jquery.com/jquery.js"></script>-->
-        <script type="text/javascript" src="<?php echo $bootstrap; ?>/js/jquery-1.11.1.js"></script>
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <!--<script type="text/javascript" src="<?php echo $bootstrap; ?>/js/jquery-1.11.1.js"></script>-->
         <script src="<?php echo $bootstrap; ?>/js/bootstrap.min.js"></script>
         <script src="<?php echo $bootstrap; ?>/js/galleria/galleria-1.3.6.min.js" type="text/javascript"></script>
         <script src="<?php echo $bootstrap; ?>/js/galleria/plugins/flickr/galleria.flickr.min.js" type="text/javascript"></script>
@@ -27,7 +27,7 @@
         <link href="<?php echo $assetUrl; ?>/css/screen.css" rel="stylesheet" type="text/css" media="screen, projection" />
         <link href="<?php echo $assetUrl; ?>/css/print.css" rel="stylesheet" type="text/css" media="print" />
         <!--[if lt IE 8]>
-            <link href="<?php echo $assetUrl; ?>/css/ie.css" rel="stylesheet" type="text/css" media="screen, projection" />
+        <link href="<?php echo $assetUrl; ?>/css/ie.css" rel="stylesheet" type="text/css" media="screen, projection" />
         <![endif]-->
         <link href="<?php echo $assetUrl; ?>/css/main.css" rel="stylesheet" type="text/css" media="all" />
         <link href="<?php echo $assetUrl; ?>/css/form.css" rel="stylesheet" type="text/css" media="all" />
@@ -36,124 +36,129 @@
             Osotto - Your High Tech Solutions
         </title>
 
-        <style>
-        body {
-            background: #FFF;
+        <script>
+        $(document).ready function(){
+            $("#image-cycle").carousel();
         }
-
-        a:visited {
-            text-decoration: none;
-        }
-        a:link {
-            text-decoration: none;
-        }
-        a:active {
-            text-decoration: none;
-        }
-        a:hover {
-            text-decoration: none;
-        }
-
-        .font-raleway {
-            font-family: 'Raleway', sans-serif;
-            -webkit-font-smoothing: antialiased;
-        }
-        .links{
-            color: #363635;
-            float: right;
-            font-family: 'Raleway',sans-serif;
-            margin-right: 20px;
-            margin-top: 13px;
-            text-transform: uppercase;
-        }
-        .btns{
-            margin-left:15px;
-        }
-        </style>
+        </script>
     </head>
 
     <body>
 
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="container">
-    <div class="pull-left">
-        <!-- <div class="logo"> -->
-            <img src="<?php echo $assetUrl; ?>/images/logo-inline.png">
-        <!-- </div> -->
-    </div>
-    <div class="pull-right links">
-        <?php
-            echo CHtml::link('Home', array('/home/'), array('class'=>'btn btn-xs btn-danger btns'));
-            echo CHtml::link('Products', array('/product/'), array('class'=>'btn btn-xs btn-danger btns'));
-            echo CHtml::link('Contact', array('#ContactUs'), array('class'=>'btn btn-xs btn-danger btns'));
-            echo CHtml::link('ABOUT US', array('#AboutUs'), array('class'=>'btn btn-xs btn-danger btns'));
-            echo CHtml::link('Register product', array('/device/regDevice'), array('class'=>'btn btn-xs btn-danger btns'));
-            if (Yii::app()->user->isGuest){
-                echo CHtml::link('Home', array('/'), array('class'=>'btn btn-xs btn-success btns'));
-                echo CHtml::link('Login', array('/login'), array('class'=>'btn btn-xs btn-success btns'));
-                // echo CHtml::link('Register', array('/account/register'), array('class'=>'btn btn-xs btn-danger btns'));
-                echo CHtml::link('Register', array('/account/register'), array('class'=>'btn btn-xs btn-danger btns'));
-            }
-            else if (!Yii::app()->user->priv>=10)
-                echo CHtml::link('Logout', array('/account/logout'), array('class'=>'btn btn-xs btn-danger btns'));
-            else if (Yii::app()->user->priv>=50){
-                echo CHtml::link('Logout', array('/account/logout'), array('class'=>'btn btn-xs btn-danger btns'));   
-                echo CHtml::link('Admin', array('/admin/default/index'), array('class'=>'btn btn-xs btn-danger btns'));
-            }
-        ?>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="col-md-10 col-md-offset-1 col-xs-12">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="<?php echo $assetUrl; ?>/images/logo-inline.png"></a>
+                </div>
 
-    </div>
-  </div>
-</nav>
-<br><br><br>
-	<?php if($this->id == 'home' && $this->action->id == 'index'): ?>
-        <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: #000;">
-    <?php else: ?>
-    	<div class="container">
-    <?php endif; ?>
-            <?php if(Yii::app()->user->hasFlash('success')): ?>
-                <br />
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="alert alert-success">
-                            <?php echo Yii::app()->user->getFlash('success'); ?>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><?php echo CHtml::link('Home', array('/home')); ?></li>
+                        <li><?php echo CHtml::link('Products', array('/product')); ?></li>
+                        <li><?php echo CHtml::link('Contact', '#ContactUs'); ?></li>
+                        <li><?php echo CHtml::link('About Us', '#AboutUs'); ?></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Link</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+
+        <br />
+
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 col-xs-12" style="padding: 0px;">
+               <?php /* <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                    <div class="container">
+                        <div class="pull-left">
+                            <!-- <div class="logo"> -->
+                            
+                            <!-- </div> -->
+                        </div>
+                        <div class="pull-right links">
+                            <?php
+                            echo CHtml::link('Home', array('/home/'), array('class'=>'btn btn-xs btn-danger btns'));
+                            echo CHtml::link('Products', array('/product/'), array('class'=>'btn btn-xs btn-danger btns'));
+                            echo CHtml::link('Contact', array('#ContactUs'), array('class'=>'btn btn-xs btn-danger btns'));
+                            echo CHtml::link('ABOUT US', array('#AboutUs'), array('class'=>'btn btn-xs btn-danger btns'));
+                            if (Yii::app()->user->isGuest){
+                                echo CHtml::link('Home', array('/'), array('class'=>'btn btn-xs btn-success btns'));
+                                echo CHtml::link('Login', array('/login'), array('class'=>'btn btn-xs btn-success btns'));
+                                // echo CHtml::link('Register', array('/account/register'), array('class'=>'btn btn-xs btn-danger btns'));
+                                echo CHtml::link('Register', array('/account/register'), array('class'=>'btn btn-xs btn-danger btns'));
+                            }
+                            else if (!Yii::app()->user->priv>=10){
+                                echo CHtml::link('Register a device', array('/device/regDevice'), array('class'=>'btn btn-xs btn-danger btns'));
+                                echo CHtml::link('Logout', array('/account/logout'), array('class'=>'btn btn-xs btn-danger btns'));   
+                            }
+                            else if (Yii::app()->user->priv>=50){
+                                echo CHtml::link('Register a device', array('/device/regDevice'), array('class'=>'btn btn-xs btn-danger btns'));
+                                echo CHtml::link('Logout', array('/account/logout'), array('class'=>'btn btn-xs btn-danger btns'));   
+                                echo CHtml::link('Admin', array('/admin/default/index'), array('class'=>'btn btn-xs btn-danger btns'));
+                            }
+                            ?>
+
                         </div>
                     </div>
-                </div>
-            <?php endif; ?>
+                </nav>
+                */ ?>
+                <?php if($this->id == 'home' && $this->action->id == 'index'): ?>
 
-            <?php echo $content; ?>
-
-            <div class="clear"></div>
-
-            <hr>
-                <div class="col-xs-12 text-center">
-                    <?php
-                        echo Yii::t(
-                            'application',
-                            'Copyright &copy; {year} by {company}.',
-                            array(
-                                '{year}' => date('Y'),
-                                '{company}' => "Nosco Systems",
-                            )
-                        );
-                    ?>
-                    <?php
-                        echo Yii::t('application', 'All rights reserved.');
-                    ?>
+                <?php else: ?>
+                    <div class="container">
+                <?php endif; ?>
+                <?php if(Yii::app()->user->hasFlash('success')): ?>
                     <br />
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="alert alert-success">
+                                <?php echo Yii::app()->user->getFlash('success'); ?>
+                            </div>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+                <?php echo $content; ?>
+
+                <div class="clear"></div>
+            </div>
+
+            <div class="col-xs-12">
+                <div class="col-xs-12 text-center" style="width:100%; background:#444; color:#CCC; padding: 30px;">
                     <?php
-                        $languages = array(
-                            'en' => 'English',
-                            'cy' => 'Cymraeg',
-                        );
-                        foreach($languages as $code => &$lang) {
-                            $lang = CHtml::link($lang, array('/language', 'lang' => $code));
-                        }
-                        echo implode(' &middot; ', $languages);
+                    echo Yii::t(
+                    'application',
+                    'Copyright &copy; {year} by {company}.',
+                    array(
+                        '{year}' => date('Y'),
+                        '{company}' => "Nosco Systems",
+                        )
+                    );
+                    ?>
+                    <?php
+                    echo Yii::t('application', 'All rights reserved.');
                     ?>
                 </div>
+            </div>
         </div>
     </body>
-
 </html>
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,184 +1,60 @@
 <?php
 /* @var $this HomeController */
 
-$this->breadcrumbs=array(
-	'Home',
-);
+	$this->breadcrumbs=array(
+		'Home',
+	);
 ?>
-<?php $assetUrl = Yii::app()->assetManager->publish(Yii::app()->theme->basePath . '/assets'); ?>
+	<?php $assetUrl = Yii::app()->assetManager->publish(Yii::app()->theme->basePath . '/assets'); ?>
+	<div style="height:400px; overflow:hidden; z-index:100;" id="image-cycle" class="carousel slide" data-ride="carousel" data-interval="4000" data-wrap="true">
+		<!-- Indicators -->
+	    <ol class="carousel-indicators">
+	        <li data-target="#image-cycle" data-slide-to="0" class="active"></li>
+	        <li data-target="#image-cycle" data-slide-to="1"></li>
+	        <li data-target="#image-cycle" data-slide-to="2"></li>
+	    </ol>
 
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" align="center">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-    <!-- <li data-target="#carousel-example-generic" data-slide-to="4"></li> -->
-  </ol>
+	    <!-- Wrapper for slides -->
+	    <div class="carousel-inner">
+			<div class="item active">
+				<img src="<?php echo $assetUrl; ?>/images/carousel-bg-dark.png" style="height:400px; width:100%" class="img-responsive">
+				<div class="carousel-caption">
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?php echo $assetUrl; ?>/images/carousel-bg-light.png" style="height:400px; width:100%" class="img-responsive">
+				<div class="carousel-caption">
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?php echo $assetUrl; ?>/images/carousel-bg-blue.png" style="height:400px; width:100%" class="img-responsive">
+				<div class="carousel-caption">
+				</div>
+			</div>
+		</div>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="<?php echo $assetUrl; ?>/images/android1.png" alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    <div class="item">
-      <img src="<?php echo $assetUrl; ?>/images/carousel2.png" alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    <div class="item">
-      <img src="<?php echo $assetUrl; ?>/images/hifi1.png" alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    <div class="item">
-      <img src="<?php echo $assetUrl; ?>/images/soundbar1.png" alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    ...
-  </div>
-  
+		<!-- Controls -->
+	    <a class="left carousel-control" href="#image-cycle" data-slide="prev">
+	        <span class="glyphicon glyphicon-chevron-left"></span>
+	    </a>
+	    <a class="right carousel-control" href="#image-cycle" data-slide="next">
+	        <span class="glyphicon glyphicon-chevron-right"></span>
+	    </a>
+	</div>
 
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
-</div>
-<div class="col-sm-12 text-center">
-  <h3 style="font-size: 4em;" class="font-ubuntu text-dark text-shadow-light">Welcome to Osotto - your high tech solutions</h3>
-</div>
-<div class="col-sm-12">
-  <div class="col-sm-6 col-xs-12 text-center" >
-    Tablets
-  <div class="col-sm-12 col-xs-12" style="z-index:999;">
-    <!-- <img src="<?php // echo Yii::app()->assetManager->publish('C:\\xampp\\htdocs\\osotto\\application\\views\\Uploads\\03bb346.png');?>" class="img-rounder img-rounded"> -->
-  </div>
-  </div>
-  <div class="col-sm-6 col-xs-12 text-center">Hifi</div>
-    <ul style="list-style-type:none;">
-
-      <li>Front 3” x2 + 1” tweeter x 2</li>
-
-      <li>Centre 3” x 2 + 1” tweeter</li>
-
-      <li>Surround 3” x 2</li>
-    
-    <li>Frequency Response: 85hz – 25khz</li>
-    </ul>
-    <p>Subwoofers:</p>
-    <ul style="list-style-type:none;">
-      <li>5.25” x 2 with side reflex ports.</li>
-
-      <li>Frequency response: 40hz – 150hz</li>
-
-      <li>S/N Ratio: >86db</li>
-  </div>
-  <div class="col-sm-6">
-    <?php echo CHtml::image($assetUrl.'/images/soundbar.png', 'alt', array ('height' => '320', 'width' => '420')); ?>
-  </div>
-  </div>
-
-<div>
-      <div class="col-md-12">
-         <div class="row text-center" id="AboutUs" style="background:lightgrey">
-          <h1 style="font-size: 4em;" class="font-ubuntu text-dark text-center text-shadow-light">About Us</h1>
-          <br>
-          Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.
-
-    Известен факт е, че читателя обръща внимание на съдържанието, което чете, а не на оформлението му. Свойството на Lorem Ipsum е, че до голяма степен има нормално разпределение на буквите и се чете по-лесно, за разлика от нормален текст на английски език като "Това е съдържание, това е съдържание". Много системи за публикуване и редактори на Уеб страници използват Lorem Ipsum като примерен текстов модел "по подразбиране", поради което при търсене на фразата "lorem ipsum" в Интернет ще бъдат открити много сайтове в процес на разработка. Някой от тези сайтове биват променяни с времето, а други по случайност или нарочно(за забавление и пр.) биват оставяни в този си незавършен вид.
-        <br /><br>
-        </div>
-        
-      </div>
-</div>
-
-  <!-- <div class="col-md-12">
-     <div class="row" id="ContactUs">
-      <h1 style="font-size: 4em;" class="font-ubuntu text-dark text-center text-shadow-light">Contact Us</h1>
-    </div>
-    <div class="col-md-3 text-center">
-      <h3 style="font-size: 1.5em" class="font-raleway text-dark">Telephone</h3>
-      02920 400998
-    </div>
-    <div class="col-md-3 text-center">
-      <h3 style="font-size: 1.5em" class="font-raleway text-dark">Email</h3>
-      sales@osotto.co.uk
-    </div>
-    <div class="col-md-3">
-
-    </div>
-    <div class="col-md-3">
-
-    </div>
-</div> -->
-
- <div>
-    <div class="content content-contact">
-      <br /><br>
-        <div class="container" id="ContactUs">
-            <br><br>
-            <h1 class="font-ubuntu text-dark text-center text-shadow-light" style="font-size: 4em;">Contact Us</h1>
-            <br /><br />
-            <div class="row">
-                <div class="col-sm-3 col-xs-6 text-center">
-                    <h3 class="font-raleway text-dark" style="font-size: 1.5em">Telephone</h3>
-                    <br />
-                    <a href="tel:02036577026">02920 400 998</a>
-                    
-                </div>
-                <div class="col-sm-3 col-xs-6 text-center">
-                    <h3 class="font-raleway text-dark" style="font-size: 1.5em">Email</h3>
-                    <br />
-                    <a href="mailto:info@smart-outsourcing-solutions.com">sales@osotto.co.uk.com</a>
-                </div>
-
-                <div class="col-sm-3 text-center">
-                    <h3 class="font-raleway text-dark" style="font-size: 1.5em">Follow Us</h3>
-                    <br />
-                    <div class="row">
-                        <div class="col-xs-4 text-right">
-                            <a href="https://www.facebook.com/pages/Smart-Outsourcing-Solutions/1446520535601324">
-                                <img src="assets/images/social/facebook.png" class="img-responsive" alt="Facebook" style="height:60px; width:60px;">
-                            </a>
-                        </div>
-                        <div class="col-xs-4 text-center">
-                            <a href="https://twitter.com/SmartOutsource_">
-                                <img src="assets/images/social/twitter.png" class="img-responsive" alt="Twitter" style="height:60px; width:60px;">
-                            </a>
-                        </div>
-                        <div class="col-xs-4 text-left">
-                            <a href="https://www.linkedin.com/company/smart-outsourcing-solutions-ltd?trk=biz-companies-cym">
-                                <img src="assets/images/social/linkedin.png" class="img-responsive" alt="Instagram" style="height:60px; width:60px;">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-</div>
-      
-        <br /><br />
-
-    </div>
-</div>
-
-
-
-<?php
-/*
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
-*/
-?>
+	<div class="row">
+		<div class="col-xs-12 col-sm-6" style="height:400px; overflow:hidden; padding-right:0px;">
+			<div style="width:100%; height:100%; background:#AAA;"><br /></div>
+		</div>
+		<div class="col-xs-12 col-sm-6" style="height:400px; overflow:hidden; padding-left:0px;">
+			<div style="width:100%; height:100%; background:#BBB;"><br /></div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-6" style="height:400px; overflow:hidden; padding-right:0px;">
+			<div style="width:100%; height:100%; background:#CCC;"><br /></div>
+		</div>
+		<div class="col-xs-12 col-sm-6" style="height:400px; overflow:hidden; padding-left:0px;">
+			<div style="width:100%; height:100%; background:#DDD;"><br /></div>
+		</div>
+	</div>

40149beb343c34eb8035c5e0e6ca5b98bf15fec8
Clive Dann <clive.dann@googlemail.com>
2014-09-11 11:59:53 +0100

    Live....

--- application/views/category/view.php
+++ application/views/category/view.php
@@ -22,7 +22,7 @@
                     </div><div class="row features">
                 <?php endif; ?>
                 <a href="<?php echo Yii::app()->urlManager->baseUrl; ?>/product/view?id=<?php echo $product->id; ?>">
-                    <div class="feature col-xs-12 col-sm-4" style="height:300px; background:#CCC">
+                    <div class="feature col-xs-12 col-sm-4" style="height:300px; background:#CCC ">
                         <?php if($product->Images && isset($product->Images[0]) && $image = $product->Images[0]): ?>
                             <?php echo CHtml::image(Yii::app()->assetManager->publish('/application/views/Uploads/330ea53.jpg'), 'Product Image', array('class' => 'img-responsive')); ?>
                         <?php endif; ?>

d5a1eb77d0d3b0b30e15ce884ede07f7af626338
Clive Dann <clive.dann@googlemail.com>
2014-09-11 12:38:33 +0100

    Live Debugging

--- application/views/category/view.php
+++ application/views/category/view.php
@@ -26,7 +26,8 @@
                 <a href="<?php echo Yii::app()->urlManager->baseUrl; ?>/product/view?id=<?php echo $product->id; ?>">
                     <div class="feature col-xs-12 col-sm-4" style="height:300px; background:#CCC ">
                         <?php if($product->Images && isset($product->Images[0]) && $image = $product->Images[0]): ?>
-                            <?php echo CHtml::image(Yii::app()->assetManager->publish($image->url), 'Product Image', array('class' => 'img-responsive')); ?>
+                            <?php var_dump(file_exists($image->url)); ?><?php var_dump($image); ?>
+                            <?php //  echo CHtml::image(Yii::app()->assetManager->publish($image->url), 'Product Image', array('class' => 'img-responsive')); ?>
                         <?php endif; ?>
                         <div class="caption">
                             <?php echo CHtml::encode(ucwords(strtolower($product->name))); ?>

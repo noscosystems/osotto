517918abe51f42f2929e4dff52847761465a7b7c
Luke Scowen <lmscowen@gmail.com>
2014-09-12 14:08:39 +0100

    Page Redesigns

--- application/views/device/notregistered.php
+++ application/views/device/notregistered.php
@@ -1,6 +1,8 @@
 <?php $baseUrl = Yii::app()->baseUrl ?>
 <meta http-equiv="refresh" content="2; url=<?php echo $baseUrl;?>/account/register">
+<br />
 <div class="alert alert-warning">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        You're not a registered user.
-    </div>
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    You must register with Osotto before you can register your device.
+    <?php echo CHtml::link('Register with Osotto', array('/account/register'), array('class' => 'alert-link')); ?>
+</div>
--- application/views/device/regDevice.php
+++ application/views/device/regDevice.php
@@ -5,81 +5,90 @@ echo $form->renderBegin();
 $widget = $form->activeFormWidget;
 ?>
 
-<div class="page-header">
-    <h1>Product registration <small>Please enter your product's details</small></h1>
-</div>
-<?php /*******************************************************************************************************
-<?php if(Yii::app()->user->hasFlash('registerSuccess')): ?>
-    <div class="alert alert-success" id="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <?php echo Yii::app()->user->getFlash('registerSuccess'); ?>
-    </div>
-<?php endif; ?>
-*******************************************************************************************************/?>
-<?php if(Yii::app()->user->hasFlash('regDevSuccess')): ?>
-    <div class="alert alert-success">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <?php echo Yii::app()->user->getFlash('regDevSuccess'); ?>
-    </div>
-<?php endif; ?>
+<br />
+<div class="col-sm-8 col-sm-offset-2">
+    <div class="well">
+        <div class="row">
+            <div class="col-sm-11 col-sm-offset-1">
+                <div class="page-header">
+                    <h1>Product registration <small>Please enter your product's details</small></h1>
+                </div>
+                <?php /*******************************************************************************************************
+                <?php if(Yii::app()->user->hasFlash('registerSuccess')): ?>
+                    <div class="alert alert-success" id="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <?php echo Yii::app()->user->getFlash('registerSuccess'); ?>
+                    </div>
+                <?php endif; ?>
+                *******************************************************************************************************/?>
+                <?php if(Yii::app()->user->hasFlash('regDevSuccess')): ?>
+                    <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <?php echo Yii::app()->user->getFlash('regDevSuccess'); ?>
+                    </div>
+                <?php endif; ?>
 
-<?php
-if($widget->errorSummary($form)){
-    echo '<div class="alert alert-danger">' . $widget->errorSummary($form) . '</div>';
-}
-?>
-<div class="row">
-    <div class="col-sm-3 control-label">Select device category:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'type', array('class' => 'form-control') ); ?>
+                <?php
+                if($widget->errorSummary($form)){
+                    echo '<div class="alert alert-danger">' . $widget->errorSummary($form) . '</div>';
+                }
+                ?>
+                <div class="row">
+                    <div class="col-sm-3 control-label">Device category:</div>
+                    <div class="col-sm-7">
+                        <?php echo $widget->input($form, 'type', array('class' => 'form-control') ); ?>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-3 control-label">Select Device:</div>
+                    <div class="col-sm-7">
+                        <?php echo $widget->input($form, 'productId', array('class' => 'form-control') ); ?>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-3 control-label">Serial Number:</div>
+                    <div class="col-sm-7">
+                        <?php echo $widget->input($form, 'serial_number', array('class' => 'form-control') ); ?>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-3 control-label">MAC Address:</div>
+                    <div class="col-sm-7">
+                        <?php echo $widget->input($form, 'MAC', array('class' => 'form-control') ); ?>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-3 control-label">Purchased From:</div>
+                    <div class="col-sm-7">
+                        <?php echo $widget->input($form, 'purchased_from', array('class' => 'form-control') ); ?>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-3 control-label">Date of Purchase:</div>
+                    <div class="col-sm-7">
+                        <?php echo $widget->input($form, 'date_purchased', array('class' => 'form-control') ); ?>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-2 col-sm-offset-3">
+                        <?php echo $widget->button($form, 'submit', array('class' => 'btn btn-lg btn-success') ); ?>
+                    </div>
+                </div>
+                <?php echo $form->renderEnd(); ?>
+                <!-- form -->
+                <?php
+                    $path = Yii::app()->assetManager->publish(Yii::getPathOfAlias('composer.twbs.bootstrap.dist'));
+                ?>
+            </div>
+        </div>
     </div>
 </div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Select your device:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'productId', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Enter your device's serial number:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'serial_number', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Enter MAC address of your device if you know it/ there is any:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'MAC', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Device purchased from:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'purchased_from', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Date of purchase:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'date_purchased', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-2 col-sm-offset-3">
-        <?php echo $widget->button($form, 'submit', array('class' => 'btn btn-lg btn-success') ); ?>
-    </div>
-</div>
-<?php echo $form->renderEnd(); ?>
-<!-- form -->
-<?php
-    $path = Yii::app()->assetManager->publish(Yii::getPathOfAlias('composer.twbs.bootstrap.dist'));
-?>
 <script type="text/javascript" src="<?php echo $path;?>/js/jquery-ui.js"></script>
 <link href="<?php echo $path; ?>/js/jquery-ui.css" rel="stylesheet" type="text/css" />
 <script>

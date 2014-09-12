29df99c1291bab2716e364c9fafc030cd945eff0
Luke Scowen <lmscowen@gmail.com>
2014-09-12 13:49:40 +0100

    Improved the login page

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -7,43 +7,47 @@
     $this->breadcrumbs = array(
         $this->pageTitle,
     );
+    $assetUrl = Yii::app()->assetManager->publish(Yii::app()->theme->basePath . '/assets');
 ?>
 <?php
     $form->attributes = array('class' => 'form-horizontal');
     echo $form->renderBegin();
     $widget = $form->activeFormWidget;
 ?>
-<br /><br /><br />
-<fieldset>
-<?php if(isset($partial)) $formLength = 9; else $formLength = 6; ?>
-    <div class="form-group <?php echo $widget->error($form, 'username') ? 'has-error' : ''; ?>">
-        <?php echo $widget->labelEx($form, 'username', array('class' => 'col-xs-3 text-right control-label')); ?>
-        <div class="col-xs-<?php echo $formLength; ?>">
-            <?php echo $widget->input($form, 'username', array('class' => 'form-control', 'autofocus' => 'true')); ?>
-            <?php
-                echo $widget->error($form, 'username', array('class' => 'help-block'))
-                  ?: $widget->hint($form, 'username', 'div', array('class' => 'help-block'));
-            ?>
+<br />
+    <div class="col-sm-6 col-sm-offset-3">
+        <div class="well">
+            <fieldset>
+                <div class="form-group <?php echo $widget->error($form, 'username') ? 'has-error' : ''; ?>">
+                    <?php echo $widget->labelEx($form, 'username', array('class' => 'col-sm-3 text-right control-label')); ?>
+                    <div class="col-sm-9">
+                        <?php echo $widget->input($form, 'username', array('class' => 'form-control', 'autofocus' => 'true')); ?>
+                        <?php
+                            echo $widget->error($form, 'username', array('class' => 'help-block'))
+                              ?: $widget->hint($form, 'username', 'div', array('class' => 'help-block'));
+                        ?>
+                    </div>
+                </div>
+
+                <div class="form-group <?php echo $widget->error($form, 'password') ? 'has-error' : ''; ?>">
+                    <?php echo $widget->labelEx($form, 'password', array('class' => 'col-sm-3 text-right control-label')); ?>
+                    <div class="col-sm-9">
+                        <?php echo $widget->input($form, 'password', array('class' => 'form-control')); ?>
+                        <?php
+                            echo $widget->error($form, 'password', array('class' => 'help-block'))
+                              ?: $widget->hint($form, 'password', 'div', array('class' => 'help-block'));
+                        ?>
+                    </div>
+                </div>
+
+                <div class="col-sm-offset-3">
+                    <?php echo $widget->button($form, 'submit', array('class' => 'btn btn-primary')); ?>
+                    <?php echo CHtml::link('Forgotten Password', array('/account/forgottenPassword'), array('class' => 'btn btn-link')); ?>
+                </div>
+
+            </fieldset>
+            <?php echo $form->renderEnd(); ?>
         </div>
     </div>
 
-    <div class="form-group <?php echo $widget->error($form, 'password') ? 'has-error' : ''; ?>">
-        <?php echo $widget->labelEx($form, 'password', array('class' => 'col-xs-3 text-right control-label')); ?>
-        <div class="col-xs-<?php echo $formLength; ?>">
-            <?php echo $widget->input($form, 'password', array('class' => 'form-control')); ?>
-            <?php
-                echo $widget->error($form, 'password', array('class' => 'help-block'))
-                  ?: $widget->hint($form, 'password', 'div', array('class' => 'help-block'));
-            ?>
-        </div>
-    </div>
-
-    <div class="col-sm-offset-3">
-        <?php echo $widget->button($form, 'submit', array('class' => 'btn btn-primary')); ?>
-        <?php echo CHtml::link('Forgotten Password', array('/account/forgottenPassword'), array('class' => 'btn btn-link')); ?>
-    </div>
-
-</fieldset>
-<?php echo $form->renderEnd(); ?>
-
-<br /><br /><br />
+<br /><br /><br /><br />

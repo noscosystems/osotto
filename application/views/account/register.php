79739ac6c8a939651f501d788e134de470f00212
Luke Scowen <lmscowen@gmail.com>
2014-09-12 13:57:37 +0100

    Re-modelled the form pages

--- application/views/account/register.php
+++ application/views/account/register.php
@@ -4,134 +4,142 @@ $form->attributes = array('class' => 'form-horizontal');
 echo $form->renderBegin();
 $widget = $form->activeFormWidget;
 ?>
+<br />
+<div class="col-sm-8 col-sm-offset-2">
+    <div class="well">
+        <div class="row">
+            <div class="col-sm-11 col-sm-offset-1">
+                <div class="page-header">
+                    <h1>Register <small>Please enter your information</small></h1>
+                </div>
+            </div>
+        </div>
 
-<div class="page-header">
-    <h1>Register <small>Please enter your information</small></h1>
-</div>
+        <?php
+        if($widget->errorSummary($form)){
+            echo '<div class="alert alert-danger">' . $widget->errorSummary($form) . '</div>';
+        }
+        ?>
 
-<?php
-if($widget->errorSummary($form)){
-    echo '<div class="alert alert-danger">' . $widget->errorSummary($form) . '</div>';
-}
-?>
-
-<div class="row">
-    <div class="col-sm-3 control-label">Enter desired Username:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'username', array('class' => 'form-control') ); ?>
+        <div class="row">
+            <div class="col-sm-3 control-label">Username:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'username', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Password:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'password', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">First Name:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'firstname', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Middle Name:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'middlename', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Last Name:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'lastname', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Age Group:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'ageGroup', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Email Address:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'email', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Mobile Number:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'mobile', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Secondary Number:</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'other_number', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Address Line 1</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'address1', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Address Line 2</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'address2', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Town</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'town', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">County</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'county', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Country</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'country', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">Postcode</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'postcode', array('class' => 'form-control') ); ?>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-3 control-label">News and Updates</div>
+            <div class="col-sm-7">
+                <?php echo $widget->input($form, 'optIn', array('class' => '', 'checked' => true) ); ?>
+                Please send me information about updates and new products
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-2 col-sm-offset-3">
+                <?php echo $widget->button($form, 'submit', array('class' => 'btn btn-lg btn-success') ); ?>
+            </div>
+        </div>
+        <?php echo $form->renderEnd(); ?>
+        <!-- form -->
     </div>
 </div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Enter your Password:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'password', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Enter your firstname here:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'firstname', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Enter your middlename here:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'middlename', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Enter your lastname here:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'lastname', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Select Age Group:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'ageGroup', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Enter your email here:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'email', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Enter your mobile number here:</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'mobile', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Fill in second phone number</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'other_number', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Addressline 1</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'address1', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Addressline2</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'address2', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Town</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'town', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">County</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'county', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Country</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'country', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">Postcode</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'postcode', array('class' => 'form-control') ); ?>
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-3 control-label">News and upates</div>
-    <div class="col-sm-6">
-        <?php echo $widget->input($form, 'optIn', array('class' => '', 'checked' => true) ); ?>
-        Please send me information about updates and new products
-    </div>
-</div>
-<br>
-<div class="row">
-    <div class="col-sm-2 col-sm-offset-3">
-        <?php echo $widget->button($form, 'submit', array('class' => 'btn btn-lg btn-success') ); ?>
-    </div>
-</div>
-<?php echo $form->renderEnd(); ?>
-<!-- form -->
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -15,39 +15,39 @@
     $widget = $form->activeFormWidget;
 ?>
 <br />
-    <div class="col-sm-6 col-sm-offset-3">
-        <div class="well">
-            <fieldset>
-                <div class="form-group <?php echo $widget->error($form, 'username') ? 'has-error' : ''; ?>">
-                    <?php echo $widget->labelEx($form, 'username', array('class' => 'col-sm-3 text-right control-label')); ?>
-                    <div class="col-sm-9">
-                        <?php echo $widget->input($form, 'username', array('class' => 'form-control', 'autofocus' => 'true')); ?>
-                        <?php
-                            echo $widget->error($form, 'username', array('class' => 'help-block'))
-                              ?: $widget->hint($form, 'username', 'div', array('class' => 'help-block'));
-                        ?>
-                    </div>
+<div class="col-sm-6 col-sm-offset-3">
+    <div class="well">
+        <fieldset>
+            <div class="form-group <?php echo $widget->error($form, 'username') ? 'has-error' : ''; ?>">
+                <?php echo $widget->labelEx($form, 'username', array('class' => 'col-sm-3 text-right control-label')); ?>
+                <div class="col-sm-9">
+                    <?php echo $widget->input($form, 'username', array('class' => 'form-control', 'autofocus' => 'true')); ?>
+                    <?php
+                        echo $widget->error($form, 'username', array('class' => 'help-block'))
+                          ?: $widget->hint($form, 'username', 'div', array('class' => 'help-block'));
+                    ?>
                 </div>
+            </div>
 
-                <div class="form-group <?php echo $widget->error($form, 'password') ? 'has-error' : ''; ?>">
-                    <?php echo $widget->labelEx($form, 'password', array('class' => 'col-sm-3 text-right control-label')); ?>
-                    <div class="col-sm-9">
-                        <?php echo $widget->input($form, 'password', array('class' => 'form-control')); ?>
-                        <?php
-                            echo $widget->error($form, 'password', array('class' => 'help-block'))
-                              ?: $widget->hint($form, 'password', 'div', array('class' => 'help-block'));
-                        ?>
-                    </div>
+            <div class="form-group <?php echo $widget->error($form, 'password') ? 'has-error' : ''; ?>">
+                <?php echo $widget->labelEx($form, 'password', array('class' => 'col-sm-3 text-right control-label')); ?>
+                <div class="col-sm-9">
+                    <?php echo $widget->input($form, 'password', array('class' => 'form-control')); ?>
+                    <?php
+                        echo $widget->error($form, 'password', array('class' => 'help-block'))
+                          ?: $widget->hint($form, 'password', 'div', array('class' => 'help-block'));
+                    ?>
                 </div>
+            </div>
 
-                <div class="col-sm-offset-3">
-                    <?php echo $widget->button($form, 'submit', array('class' => 'btn btn-primary')); ?>
-                    <?php echo CHtml::link('Forgotten Password', array('/account/forgottenPassword'), array('class' => 'btn btn-link')); ?>
-                </div>
+            <div class="col-sm-offset-3">
+                <?php echo $widget->button($form, 'submit', array('class' => 'btn btn-primary')); ?>
+                <?php echo CHtml::link('Forgotten Password', array('/account/forgottenPassword'), array('class' => 'btn btn-link')); ?>
+            </div>
 
-            </fieldset>
-            <?php echo $form->renderEnd(); ?>
-        </div>
+        </fieldset>
+        <?php echo $form->renderEnd(); ?>
     </div>
+</div>
 
 <br /><br /><br /><br />

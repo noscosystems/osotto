74a3832f5a19d4d1266033a439b4789f4f954dfe
Luke Scowen <lmscowen@gmail.com>
2014-11-19 15:38:08 +0000

    Username in the top bar

--- application/themes/classic/views/layouts/main.php
+++ application/themes/classic/views/layouts/main.php
@@ -90,12 +90,12 @@ $bootstrap = Yii::app()->assetManager->publish(Yii::getPathOfAlias('composer.twb
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
                             <?php if(Yii::app()->user->isGuest): ?>
-                                <li><?php echo CHtml::link('Register Device', array('/device/regdevice')); ?></li>
+                                <li><?php echo CHtml::link('Register Device', array('/device/register')); ?></li>
                                 <li><?php echo CHtml::link('Login', array('/login')); ?></li>
                             <?php elseif(Yii::app()->user->priv >= 10): ?>
                                 <li><?php echo CHtml::link('Register Device', array('/device/regdevice')); ?></li>
                                 <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account <span class="caret"></span></a>
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo CHtml::encode(Yii::app()->user->model()->username); ?> <span class="caret"></span></a>
                                     <ul class="dropdown-menu" role="menu">
                                         <?php if(Yii::app()->user->priv >= 50): ?>
                                             <li><?php echo CHtml::link('Admin', array('/admin/default/index')); ?></li>
